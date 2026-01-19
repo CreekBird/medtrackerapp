55f8a89b11f349c517f46bec881fc2b4a297a88a
George <283089@student.pwr.edu.pl>
2026-01-19 19:11:59 +0100

    Fix Ruff linting issues

--- medtrackerapp/views.py
+++ medtrackerapp/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, status
+from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from django.utils.dateparse import parse_date
